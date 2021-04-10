29656ce84facb489bdffe4ff421290bd4eb9b1ee
Skyler Carlson <skylercarlson28@gmail.com>
2021-04-10 12:21:26 -0700

    Save to directory with name of noise ratio value

--- screen_ratio.py
+++ screen_ratio.py
@@ -16,17 +16,19 @@ screen_parser.add_argument('--device', type=str, default='cuda', choices=['cuda'
                         help='cuda or cpu')
 screen_parser.add_argument('--noise_interval', type=float, default=0.0005,
                         help='noise interval')
-screen_parser.add_argument('--save_str', type=str, default=None,
-                        help='subdir to store results in')
 screen_args = screen_parser.parse_args()
 
 ratios = np.linspace(0.1, 1, 10)
 
 for rat in ratios:
     print(f'----- Running noise_ratio = {rat} -----')
-    sys.argv.extend(['--noise_ratio', str(np.round(rat, 4))])
+    sys.argv.extend(['--noise_ratio', str(np.round(rat, 4)), '--save_str', f'nr_{str(np.round(rat, 4))}'])
     screen_main()
 
     idx = sys.argv.index('--noise_ratio')
+    del sys.argv[idx+1]
+    del sys.argv[idx]
+
+    idx = sys.argv.index('--save_str')
     del sys.argv[idx+1]
     del sys.argv[idx]
